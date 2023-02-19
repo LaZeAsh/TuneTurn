02e9103fee4f4be3c0ecbca83cc7edb66efbe900
Hong-ut <7594hsj@gmail.com>
2023-02-18 20:17:09 -0500

    backend

--- backend.py
+++ backend.py
@@ -8,12 +8,13 @@ app = Flask(__name__)
 def action(left_or_right):
     if left_or_right == "left":
         # return left arrow key
+        time.sleep(1)
         pyautogui.press('left', presses = 1)
 
         return jsonify({'message' : 'left'})
     elif left_or_right == "right":
         # return right arrow key
-        time.sleep(3)
+        time.sleep(1)
         pyautogui.press('right', presses = 1)
         return jsonify({'message' : 'right'})
 
